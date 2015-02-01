749bd0456bee1cd230b02c59c50d15372ea67a5b
Jake Klassen <jklassendev@gmail.com>
2015-02-01 07:20:52 -0500

    clamp

---
+++ number/clamp.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Clamp function
+ */
+
+/**
+ * Returns a number whose value is limited to the given range.
+ *
+ * @param {Number} num The number to clamp
+ * @param {Number} min The lower boundary of the output range
+ * @param {Number} max The upper boundary of the output range
+ * @returns A number in the range [min, max]
+ */
+module.exports = function(num, min, max) {
+  return Math.min(Math.max(num, min), max);
+};
